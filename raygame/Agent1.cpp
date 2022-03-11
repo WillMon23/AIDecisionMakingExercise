9654d032b7cc452e50f983fffe342e68a84f944f
Will <montero_w@live.com>
2022-03-11 11:48:36 -0600

    Added New Component

--- raygame/Agent1.cpp
+++ raygame/Agent1.cpp
@@ -1,11 +1,12 @@
 #include "Agent1.h"
+#include "Agent.h"
 #include "SpriteComponent.h"
 #include "Goal.h"
 #include "GameManager.h"
 
 Agent1::Agent1(float x, float y, const char* name, float maxForce, float maxSpeed, float health) : Character(x, y, name, maxForce, maxSpeed, health)
 {
-	addComponent(new SpriteComponent("Images/player.png"));
+	addComponent(new SpriteComponent("Images/newShip.png"));
 }
 
 void Agent1::onCollision(Actor* actor)
@@ -22,7 +23,6 @@ void Agent1::start()
 void Agent1::update(float deltaTime)
 {
 	Character::update(deltaTime);
-
 }
 
 void Agent1::onDeath()
---
+++ raygame/StateMachineWMComponent.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Component.h"
+class StateMachineWMComponent :
+    public Component
+{
+public:
+    void start() override;
+    void update(float deltatime);
+};
+
